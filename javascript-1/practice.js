25ca3ab2f8104b7bda3309f490fb58ab562b97f8
Scorpioeric-dev <ericscorpio1@gmail.com>
2019-08-22 17:43:01 -0600

    sim

--- javascript-1/practice.js
+++ javascript-1/practice.js
@@ -14,7 +14,7 @@ let faveNum = 11;
 
 // Create a variable called 'lovesCode' and set it to true
 
-let lovesCode = 'true';
+let lovesCode = true;
 //////////////////PROBLEM 4////////////////////
 
 // Create a function called 'sum' that takes in two parameters, num1 and num2, and returns the sum of those two parameters.
@@ -28,7 +28,11 @@ sum(2, 4);
 // Create a function called 'lovesCodeChecker' that takes in a single parameter called x. Check to see if x is equal to true. If it is, return true as a boolean. If x does not equal true, return false as a boolean
 
 function lovesCodeChecker(x){
-	if()
+	if(x === true){
+		return true
+	} else {
+		return false
+	}
 }
 
 //////////////////PROBLEM 6////////////////////
@@ -36,7 +40,11 @@ function lovesCodeChecker(x){
 // Create a function called 'oddChecker' that takes in one parameter, num. Check to see if the number is even or odd. If it is odd, return 'the number is odd' or return 'the number is even' if it is even.
 
 function oddChecker(num){
-
+// if(num%2){
+// 	return ' the number is even'
+// } else {
+// 	return ' the number is odd'
+// }
 }
 
 //////////////////PROBLEM 7////////////////////
@@ -44,8 +52,13 @@ function oddChecker(num){
 // Create a function called 'iLove' that takes in two parameters, name which will be a string  and love which will be a string of something you love. Have the function take the two parameters and retrun a string that says "NAMEPARAM loves LOVEPARAM" with the appropriate parameters in the string. e.g. "Bryan loves skateboarding"
 
 function iLove(name,love){
+//  let name = ' Bryan'
+//  let love =  'Skateboarding'
+
 
 }
+
+
 
 //////////////////PROBLEM 8////////////////////
 
@@ -90,27 +103,28 @@ console.log(me)
 // Create a function called 'bigOrSmall' that takes in one parameter, arr. Create a new array inside of bigOrSmall called 'answers'. Loop over the passed in arr param. If the number is GREATER than 100, push 'big' as a string to the answers array. If the number is LESS than or EQUAL to 100, push 'small' as a string to the answers array. Return the answers array inside of the function
 
 function bigOrSmall(arr){
-	let answers = [array];
+	let answers = [];
 	for(i=0;i<arr.length;i++){
 	 if (i<100){
 return answers.push('big')
-	 } if(i>100) {
+	 } if(i>=100) {
 	
 return answers.push('small')
 	 }
 	}
 }
-console.log(answers)
+// console.log(bigOrSmall)
 //////////////////PROBLEM 13////////////////////
 
 // Create a function called 'arrayReverser' that takes in one parameter, arr. Inside of arrayReverser, create an empty array called 'reversed'. Using a for loop, loop over the passed in array in reverse and add each item to the new reversed array. Finally, return the new reversed array
 
 function arrayReverser(arr){
 	let reversed = []
-	for(i=0;i<arr,reverse;i++){
-
+	for(i=0; i < arr.reverse ; i++){
+    return arr.push(reversed) 
 	}
 }
+
 
 //////////////////PROBLEM 14 - 18 Setup////////////////////
 
@@ -132,20 +146,23 @@ console.log(doubled)
 // Now lets use .filter to only get the nubmer(s) greater than 100. Call the new array 'filtered'
 
 const filtered = myNumbers.filter(function(number){
-	return myNumbers < 100;
+	return number > 100;
 })
 console.log(filtered)
 //////////////////PROBLEM 16////////////////////
 
 // Next up, reduce to get the total of the array. call the new array 'total'
+myNumbers.reduce()
 
 
 
 //////////////////PROBLEM 17////////////////////
 
 // Last, lets use .forEach to find the index of each item in the array. First, create an empty array called myNumbersIndex. Next, using forEach, push each items index from the myNumbers array to the myNumbersIndex array
-let MyNumbersIndex = [];
-
+let myNumbersIndex = [];
+myNumbers.forEach(function(myNumbersIndex){
+	console.log(myNumbersIndex)
+})
 //////////////////PROBLEM 18////////////////////
 
 // Did you know that George Foreman has five sons named George? Let's go ahead and change everyone's name in the notGeorge array to George using .map. Call the new array 'forTheLoveOfGeorge'
